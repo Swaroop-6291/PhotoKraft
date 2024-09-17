33211f3751e6c1e369e87ec675bf9d8205d9f51b
Swaroop-6291 <swaroop6291@gmail.com>
2024-09-17 19:48:12 +0530

    Second commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationUpdatePage = () => {
+  return (
+    <div>
+      TransformationUpdate
+    </div>
+  )
+}
+
+export default TransformationUpdatePage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformationAdd = () => {
+  return (
+    <div>TransformationAdd</div>
+  )
+}
+
+export default TransformationAdd
